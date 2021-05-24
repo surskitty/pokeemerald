2ae0fbc7bfd8817a4476d7845a62de0adfe357a2
surskitty <surskitty@gmail.com>
2021-05-24 17:32:18 -0400

    Refactor + bug fix.

--- include/constants/trainers.h
+++ include/constants/trainers.h
@@ -375,4 +375,19 @@
 #define F_TRAINER_PARTY_HELD_ITEM      (1 << 1)
 #define F_TRAINER_PARTY_CUSTOM_MON     (1 << 2)
 
+#define TRAINER_MON_DEFENSES   1
+#define TRAINER_MON_SPEED_PHYS 2
+#define TRAINER_MON_SPEED_SPEC 3
+#define TRAINER_MON_HP_DEF     4
+#define TRAINER_MON_HP_SPDEF   5
+#define TRAINER_MON_PHYS       6
+#define TRAINER_MON_SPEC       7
+
+#define TRAINER_MON_FEMALE     1
+#define TRAINER_MON_MALE       2
+
+#define TRAINER_MON_FRIENDLY     1
+#define TRAINER_MON_UNFRIENDLY   2
+
+
 #endif  // GUARD_TRAINERS_H
